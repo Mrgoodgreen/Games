ba93c903686d8b3eeb0f1d49542be3591dfae75f
Mrgoodgreen <romanenko.inc@gmail.com>
2026-03-15 16:06:53 +0300

    dz9upd

--- ArkanoidGame/GameStatePlaying.cpp
+++ ArkanoidGame/GameStatePlaying.cpp
@@ -337,6 +337,7 @@ namespace ArkanoidGame
                     }
 
                     it = bricks.erase(it);
+                    createLifeMemento();
                 }
                 else
                 {
